f880d18fea7f822bf2faa8988da13a043f0846ad
Melchor Gabriel Lopez Castro <48580537+ADH-MelchorGabriel@users.noreply.github.com>
2026-05-02 20:07:21 -0700

    Add initial Diplomado-Tareas.API project
    Add a new .NET 6 Web API project/solution for Diplomado-Tareas.API. Includes the solution and project files, Program.cs, appsettings (Development and default), launchSettings, and model classes (EstadoTareaEnum, TareaEntity). The changeset also contains Visual Studio metadata and generated build outputs (.vs, bin/, obj/) created by the IDE.

---
+++ BackEnd/Diplomado-Tareas.API/Diplomado-Tareas.API/Models/Tareas/EstadoTareaEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace Tareas.Enumeradores
+{
+    public enum EstadoTareaEnum
+    {
+        Pendiente = 1,
+        Retrazado = 2,
+        Terminado = 3,
+
+        Cancelado = 99
+    }
+}
